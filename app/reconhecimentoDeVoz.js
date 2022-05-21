ace04779994a08b6fc9fe940493440a58dfa1617
Guilherme Lima <guilherme.lima@alura.com>
2022-05-21 16:02:09 -0300

    video3.2

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -3,3 +3,9 @@ window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    console.log(e.results[0][0].transcript)
+}
