ab0af8f9d5683cc40d311d7b0fefbdfa272f7ba0
Guilherme Lima <guilherme.lima@alura.com>
2022-05-21 16:55:26 -0300

    video3.3

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute')
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -7,5 +9,13 @@ recognition.start()
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    console.log(e.results[0][0].transcript)
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
